3d3967b87c9338100ff01add2146568bf77bc5b5
hollowness-inside-pc <malikov.vp@students.dvfu.ru>
2024-03-04 01:09:26 +1000

    Implement decryption algorithm

--- src/rsa.rs
+++ src/rsa.rs
@@ -28,3 +28,12 @@ pub struct RsaPrivate {
     pub prime_1: UBig,
     pub prime_2: UBig,
 }
+
+impl RsaPrivate {
+    pub fn decrypt(&self, message: &UBig) -> UBig {
+        let exp = self.private_exponent.clone();
+        let div = &self.prime_1 * &self.prime_2;
+
+        message.powmod(exp, &div)
+    }
+}
