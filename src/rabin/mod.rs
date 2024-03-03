3636a1b8c0d9738b11d38786360d642249449c66
hollowness-inside-pc <malikov.vp@students.dvfu.ru>
2024-03-04 02:06:40 +1000

    pub use symbols

--- src/rabin/mod.rs
+++ src/rabin/mod.rs
@@ -2,4 +2,8 @@ mod pair;
 mod private;
 mod public;
 
-pub(crate) const MAGIC: &[u8; 8] = b"\x00h&\x98\x8c\xef\x18\xff";
+pub(crate) const MAGIC: &[u8; 8] = b"\x00RABIN\x00\x01";
+
+pub use pair::RabinKeyPair;
+pub use private::RabinPrivate;
+pub use public::RabinPublic;
