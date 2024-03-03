024fbdce269e6a226bfb4ce9e49557d93e603166
hollowness-inside-pc <malikov.vp@students.dvfu.ru>
2024-03-04 01:07:14 +1000

    Implement encryption algorithm

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,23 @@
+mod result;
+use crate::result::{Result, Error};
+
 use ibig::UBig;
+use powmod::PowMod;
 
 #[derive(Debug)]
 pub struct RsaPublic {
     pub exponent: UBig,
     pub divisor: UBig,
 }
+
+impl RsaPublic {
+    pub fn encrypt(&self, bytes: &[u8]) -> Result<UBig> {
+        let message = UBig::from_le_bytes(bytes);
+        if message > self.divisor {
+            return Err(Error::SmallKey);
+        }
+
+        let exp = self.exponent.clone();
+        Ok(message.powmod(exp, &self.divisor))
     }
 }
