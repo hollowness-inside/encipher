d5183bcf6e7bdf10ffefcf431b6d048d159c1d5a
hollowness-inside-pc <malikov.vp@students.dvfu.ru>
2024-03-04 01:58:31 +1000

    Implement key generation

--- src/rabin/pair.rs
+++ src/rabin/pair.rs
@@ -1,3 +1,5 @@
+use ibig::UBig;
+
 use super::private::RabinPrivate;
 use super::public::RabinPublic;
 
@@ -6,4 +8,32 @@ pub struct RabinKeyPair {
     pub public: RabinPublic,
     pub private: RabinPrivate,
     pub version: usize,
+}
+
+impl RabinKeyPair {
+    pub fn new(mut bit_length: usize, persistence: usize) -> Self {
+        bit_length += 8;
+
+        let p = gen_prime(bit_length, persistence);
+        let q = gen_prime(bit_length, persistence);
+        let n = &p * &q;
+
+        Self {
+            public: RabinPublic { divisor: n },
+            private: RabinPrivate {
+                prime_1: p,
+                prime_2: q,
+            },
+            version: 0,
+        }
+    }
+}
+
+fn gen_prime(byte_length: usize, persistence: usize) -> UBig {
+    loop {
+        let p = prime_gen::gen_sized_prime(byte_length, persistence);
+        if &p % 4 == 3 {
+            break p;
+        }
+    }
 }
