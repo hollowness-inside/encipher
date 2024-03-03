3ed8df19ef455213743eb2f2a2f602e1d6f56284
hollowness-inside-pc <malikov.vp@students.dvfu.ru>
2024-03-04 02:14:24 +1000

    Implement key generation

--- src/elgamal/pair.rs
+++ src/elgamal/pair.rs
@@ -1,3 +1,8 @@
+use ibig::ubig;
+use powmod::PowMod;
+use prime_gen::gen_sized_prime;
+use rand::Rng;
+
 use super::ElGamalPrivate;
 use super::ElGamalPublic;
 
@@ -6,4 +11,32 @@ pub struct ElGamalKeyPair {
     pub public: ElGamalPublic,
     pub private: ElGamalPrivate,
     pub chunk_size: usize,
+}
+
+impl ElGamalKeyPair {
+    pub fn new(bit_length: usize, persistence: usize) -> Self {
+        let mut rng = rand::thread_rng();
+
+        let prime = gen_sized_prime(bit_length, persistence);
+        let key = rng.gen_range(ubig!(1)..=&prime - 2);
+
+        let alpha = loop {
+            let i = rng.gen_range(ubig!(3)..=&prime - 2);
+            if i.powmod(&prime - 1, &prime) == ubig!(1) {
+                break i;
+            }
+        };
+
+        let beta = alpha.powmod(key.clone(), &prime);
+
+        Self {
+            public: ElGamalPublic {
+                prime: prime.clone(),
+                alpha,
+                beta,
+            },
+            private: ElGamalPrivate { prime, key },
+            chunk_size: 16,
+        }
+    }
 }
