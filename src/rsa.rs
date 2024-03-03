8dafa0f53f794500f49b4d00223fd260c859ce83
hollowness-inside-pc <malikov.vp@students.dvfu.ru>
2024-03-04 01:14:02 +1000

    Implement key generation algorithm

--- src/rsa.rs
+++ src/rsa.rs
@@ -1,6 +1,6 @@
 use crate::result::{Error, Result};
 
-use ibig::UBig;
+use ibig::{ubig, UBig};
 use powmod::PowMod;
 
 #[derive(Debug)]
@@ -41,3 +41,28 @@ impl RsaPrivate {
 #[derive(Debug)]
 pub struct RsaKeyPair(pub RsaPublic, pub RsaPrivate);
 
+impl RsaKeyPair {
+    pub fn new(bit_length: usize, persistence: usize) -> Self {
+        let p = prime_gen::gen_sized_prime(bit_length, persistence);
+        let q = prime_gen::gen_sized_prime(bit_length, persistence);
+
+        let n = &p * &q;
+        let totient = (&p - 1) * (&q - 1);
+
+        let e: UBig = ubig!(2).pow(16) + 1;
+        let (_, d, _) = e.extended_gcd(&totient);
+        let d: UBig = d.try_into().expect("Cannot convert d to UBig");
+
+        Self(
+            RsaPublic {
+                exponent: e,
+                divisor: n,
+            },
+            RsaPrivate {
+                private_exponent: d.clone(),
+                prime_1: p.clone(),
+                prime_2: q.clone(),
+            },
+        )
+    }
+}
