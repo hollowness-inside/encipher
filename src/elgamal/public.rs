24d5f33173342314ebf073033e91e618c2659e49
hollowness-inside-pc <malikov.vp@students.dvfu.ru>
2024-03-04 02:11:22 +1000

    Implement encryption

--- src/elgamal/public.rs
+++ src/elgamal/public.rs
@@ -1,8 +1,29 @@
-use ibig::UBig;
+use ibig::{ubig, UBig};
+use powmod::PowMod;
+use rand::Rng;
+
+use crate::result::{Error, Result};
 
 #[derive(Debug)]
 pub struct ElGamalPublic {
     pub prime: UBig,
     pub alpha: UBig,
     pub beta: UBig,
+}
+
+impl ElGamalPublic {
+    pub fn encrypt(&self, bytes: &[u8]) -> Result<[UBig; 2]> {
+        let message = UBig::from_le_bytes(bytes);
+        if message > self.prime {
+            return Err(Error::SmallKey);
+        }
+
+        let mut rng = rand::thread_rng();
+        let r = rng.gen_range(ubig!(0)..=&self.prime - 2);
+
+        let c1 = self.alpha.powmod(r.clone(), &self.prime);
+        let c2 = message * self.beta.powmod(r, &self.prime);
+
+        Ok([c1, c2])
+    }
 }
