1498c377603c8182ce1d51e1fb2bbc2d50a3b232
hollowness-inside-pc <malikov.vp@students.dvfu.ru>
2024-03-04 01:45:58 +1000

    Implement encryption

--- src/rabin/mod.rs
+++ src/rabin/mod.rs
@@ -1,3 +1,5 @@
 mod pair;
 mod private;
 mod public;
+
+pub(crate) const MAGIC: &[u8; 8] = b"\x00h&\x98\x8c\xef\x18\xff";
--- src/rabin/public.rs
+++ src/rabin/public.rs
@@ -1,6 +1,26 @@
-use ibig::UBig;
+use ibig::{ubig, UBig};
+use powmod::PowMod;
+
+use crate::result::{Error, Result};
+
+use super::MAGIC;
 
 #[derive(Debug)]
 pub struct RabinPublic {
     pub divisor: UBig,
+}
+
+impl RabinPublic {
+    pub fn encrypt(&self, message: &[u8]) -> Result<UBig> {
+        let mut message = message.to_vec();
+        message.extend(MAGIC);
+
+        let message = UBig::from_le_bytes(&message);
+
+        if message >= self.divisor {
+            return Err(Error::SmallKey);
+        }
+
+        Ok(message.powmod(ubig!(2), &self.divisor))
+    }
 }
