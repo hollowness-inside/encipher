d219eab361dc694859a121a2d2d402bc7e9ba39e
hollowness-inside-pc <malikov.vp@students.dvfu.ru>
2024-03-04 01:06:29 +1000

    Create struct for RSA public key

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,9 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use ibig::UBig;
+
+#[derive(Debug)]
+pub struct RsaPublic {
+    pub exponent: UBig,
+    pub divisor: UBig,
 }
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
     }
 }
