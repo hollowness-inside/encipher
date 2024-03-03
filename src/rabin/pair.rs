0b28c35108ed2f3b82cf044dfdf71914f28b2144
hollowness-inside-pc <malikov.vp@students.dvfu.ru>
2024-03-04 01:57:10 +1000

    Create `RabinKeyPair`

--- src/rabin/pair.rs
+++ src/rabin/pair.rs
@@ -1,1 +1,9 @@
+use super::private::RabinPrivate;
+use super::public::RabinPublic;
 
+#[derive(Debug)]
+pub struct RabinKeyPair {
+    pub public: RabinPublic,
+    pub private: RabinPrivate,
+    pub version: usize,
+}
