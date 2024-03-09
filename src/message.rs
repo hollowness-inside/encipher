c228603f1e4a0a75147e3951f59b14e021e0c1c3
hollowness-inside-pc <malikov.vp@students.dvfu.ru>
2024-03-09 11:28:12 +1000

    Add optional serde support

--- src/message.rs
+++ src/message.rs
@@ -2,6 +2,7 @@ use ibig::UBig;
 
 /// A container for encrypted message.
 #[derive(Debug)]
+#[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
 pub struct Message {
     /// The type of the message content.
     pub content_type: ContentType,
@@ -12,6 +13,7 @@ pub struct Message {
 
 /// Different formats for encrypted message content depending on the chosen algorithm.
 #[derive(Debug)]
+#[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
 pub enum Content {
     /// RSA encryption content containing the chunk size and encrypted message blocks.
     Rsa(usize, Vec<UBig>),
@@ -25,6 +27,7 @@ pub enum Content {
 
 /// Supported content types for messages.
 #[derive(Debug, PartialEq)]
+#[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
 pub enum ContentType {
     Text,
     Bytes,
