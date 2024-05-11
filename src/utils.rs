c1aaaa1fb0b79805f43f5421999bcc1951891100
hollowness-inside-pc <malikov.vp@students.dvfu.ru>
2024-05-11 23:15:52 +1000

    Add docstrings

--- src/utils.rs
+++ src/utils.rs
@@ -50,6 +50,20 @@ pub(crate) fn unpad_message(bytes: &[u8], block_size: usize) -> &[u8] {
     &bytes[0..len - block_size - pad_len]
 }
 
+/// Marshals a vector of vectors of bytes into a single byte vector.
+///
+/// Each inner vector represents a chunk of bytes to be marshalled.
+/// The resulting byte vector is formatted such that each chunk is prefixed
+/// with an 8-byte length field in big-endian format.
+///
+/// # Arguments
+///
+/// * `bytes` - A reference to a vector of vectors of bytes to be marshalled.
+///
+/// # Returns
+///
+/// A byte vector containing the marshalled bytes.
+///
 pub(crate) fn marshal_bytes(bytes: &Vec<Vec<u8>>) -> Vec<u8> {
     let byte_length = bytes.iter().fold(0, |acc, e| acc + e.len());
     let mut result = Vec::with_capacity(byte_length + bytes.len() * 8);
@@ -63,6 +77,20 @@ pub(crate) fn marshal_bytes(bytes: &Vec<Vec<u8>>) -> Vec<u8> {
     result
 }
 
+/// Unmarshals a slice of raw bytes into a vector of vectors of bytes.
+///
+/// Each inner vector represents a chunk of bytes extracted from the raw byte slice.
+/// The input `raw_bytes` slice is expected to be formatted such that each chunk is prefixed
+/// with an 8-byte length field in big-endian format.
+///
+/// # Arguments
+///
+/// * `raw_bytes` - A slice of raw bytes to be unmarshalled.
+///
+/// # Returns
+///
+/// A vector of vectors of bytes, where each inner vector contains a chunk of bytes.
+///
 pub(crate) fn unmarshal_bytes(raw_bytes: &[u8]) -> Vec<Vec<u8>> {
     let mut result = Vec::with_capacity(raw_bytes.len());
 
