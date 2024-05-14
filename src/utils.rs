369e2e7da9aecf690568b212b5704d22f731ca46
hollowness-inside-pc <malikov.vp@students.dvfu.ru>
2024-05-15 00:25:36 +1000

    Write `test_marshal_unmarshal`

--- src/utils.rs
+++ src/utils.rs
@@ -106,3 +106,20 @@ pub(crate) fn unmarshal_bytes(raw_bytes: &[u8]) -> Vec<Vec<u8>> {
 
     result
 }
+
+#[cfg(test)]
+mod tests {
+    use super::{marshal_bytes, unmarshal_bytes};
+
+    const MESSAGE: [u8; 432] = *b"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id";
+
+    #[test]
+    fn test_marshal_unmarshal() {
+        let chunks: Vec<Vec<u8>> = MESSAGE.chunks(64).map(|chunk| chunk.to_vec()).collect();
+
+        let marshalled = marshal_bytes(&chunks);
+        let unmarshalled: Vec<u8> = unmarshal_bytes(&marshalled).into_iter().flatten().collect();
+
+        assert_eq!(MESSAGE, unmarshalled.as_slice());
+    }
+}
