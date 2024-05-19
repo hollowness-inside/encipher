653453ef493c2f189f03369e1b82b26a594eebb1
hollowness-inside-pc <malikov.vp@students.dvfu.ru>
2024-05-19 23:38:30 +1000

    Fix formatting

--- examples/rabin.rs
+++ examples/rabin.rs
@@ -5,7 +5,7 @@ fn main() {
     println!("{key:#?}\n");
 
     let encrypted = key.encrypt("Hello World", 16).unwrap();
-    println!("{encrypted:#?}\n");
+    println!("{encrypted:?}\n");
 
     let decrypted = key.decrypt(&encrypted, 16).unwrap();
     println!("{:#?}\n", String::from_utf8_lossy(&decrypted));
