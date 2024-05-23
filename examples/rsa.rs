58349b08a5003fae9672daae2e29a30cd7bdd7af
hollowness-inside-pc <malikov.vp@students.dvfu.ru>
2024-05-23 22:05:38 +1000

    Update example

--- examples/rsa.rs
+++ examples/rsa.rs
@@ -10,4 +10,12 @@ fn main() {
 
     let decrypted = key.decrypt_chunked(&encrypted, 16).unwrap();
     println!("{:#?}\n", String::from_utf8_lossy(&decrypted));
+
+    let encrypted = key.public().encrypt_chunked(b"Hello World", 16).unwrap();
+    let decrypted = key.private().decrypt_chunked(&encrypted, 16).unwrap();
+    println!("{:#?}\n", String::from_utf8_lossy(&decrypted));
+
+    let encrypted = key.private().encrypt_chunked(b"Hello World", 16).unwrap();
+    let decrypted = key.public().decrypt_chunked(&encrypted, 16).unwrap();
+    println!("{:#?}\n", String::from_utf8_lossy(&decrypted));
 }
