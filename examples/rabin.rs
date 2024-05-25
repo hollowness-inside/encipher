3bcb635efa0a5dff262a1b142a3df2f954a8a8c0
hollowness-inside-pc <malikov.vp@students.dvfu.ru>
2024-05-25 22:21:33 +1000

    Update examples

--- examples/rabin.rs
+++ examples/rabin.rs
@@ -2,12 +2,24 @@ use encipher::{keypair::{KeyPair, PrivateKey, PublicKey},
                rabin::RabinKeyPair};
 
 fn main() {
+    let message = b"Hello World";
+
     let key = RabinKeyPair::generate(128, 10);
     println!("{key:#?}\n");
 
-    let encrypted = key.encrypt_chunked(b"Hello World", 16).unwrap();
-    println!("{encrypted:?}\n");
-
+    // Using key directly
+    let encrypted = key.encrypt_chunked(message, 16).unwrap();
     let decrypted = key.decrypt_chunked(&encrypted, 16).unwrap();
+    println!("{encrypted:?}\n");
     println!("{:#?}\n", String::from_utf8_lossy(&decrypted));
+
+    // Using public and private keys explicitly
+    let encrypted = key.public().encrypt_chunked(message, 16).unwrap();
+    let decrypted = key.private().decrypt_chunked(&encrypted, 16).unwrap();
+    println!("{:#?}\n", String::from_utf8_lossy(&decrypted));
+
+    // Sign data
+    let signed = key.sign_chunked(message, 8).unwrap();
+    let verification = key.verify_chunked(message, &signed, 8).unwrap();
+    println!("{verification}");
 }
