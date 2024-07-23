58fbfe5c120aa4ca1c49215cc778c3bcd3f44b49
hollowness-inside <malikov.vp@students.dvfu.ru>
2024-07-23 23:01:19 +1000

    Format

--- examples/rsa.rs
+++ examples/rsa.rs
@@ -6,7 +6,6 @@ fn main() {
     let key = RsaKeyPair::new(128, 10);
     println!("{key:#?}\n");
 
-    // Using key directly
     let encrypted = key.encrypt_chunked(message, 16).unwrap();
     let decrypted = key.decrypt_chunked(&encrypted, 16).unwrap();
     println!("{encrypted:?}\n");
