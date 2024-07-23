0f1db4e33fb2b916f55927315c75ae6469068bcd
hollowness-inside <malikov.vp@students.dvfu.ru>
2024-07-23 23:40:21 +1000

    Simplify `decrypt`

--- src/rabin/private.rs
+++ src/rabin/private.rs
@@ -3,6 +3,7 @@ use ibig_ext::sqrt::SquareRootMod;
 
 use super::MAGIC;
 use crate::result::Error;
+use crate::utils::imod;
 use crate::{keypair::PrivateKey, result::Result};
 
 /// Private key for the Rabin cryptosystem.
@@ -24,17 +25,20 @@ impl PrivateKey for RabinPrivate {
 
         let (_, u, v) = p1.extended_gcd(p2);
         let message = UBig::from_be_bytes(message);
-        let u = UBig::try_from(u).expect("Cannot convert u to UBig");
-        let v = UBig::try_from(v).expect("Cannot convert v to UBig");
+        let u = imod(&u, p2);
+        let v = imod(&v, p2);
 
         let mp1: UBig = message.clone().square_root_mod(p1).expect("No root").0;
         let mp2: UBig = message.square_root_mod(p2).expect("No root").0;
 
         let n = p1 * p2;
-        let m1: UBig = (&u * p1 * &mp2 + &v * p2 * &mp1) % &n;
+        let x1 = &u * p1 * &mp2;
+        let x2 = &v * p2 * &mp1;
+
+        let m1: UBig = (&x1 + &x2) % &n;
         let m2: UBig = &n - &m1;
-        let m3: UBig = (&u * p1 * &mp2 - &v * p2 * &mp1) % &n;
-        let m4: UBig = &n - &m3;
+        let m3: UBig = (&x1 - &x2) % &n;
+        let m4: UBig = n - &m3;
 
         for m in [m1, m2, m3, m4] {
             let m = m.to_be_bytes();
--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -1,11 +1,16 @@
 mod marshal;
 mod to_bytes;
 
+use ibig::ops::RemEuclid;
 use ibig::{IBig, UBig};
 pub(crate) use marshal::{marshal_bytes, unmarshal_bytes};
 
 pub(crate) fn imod_inverse(a: &UBig, m: &UBig) -> IBig {
     a.extended_gcd(m).1
+}
+
+pub(crate) fn imod(a: &IBig, m: &UBig) -> UBig {
+    UBig::try_from(a.rem_euclid(IBig::from(m))).unwrap()
 }
 
 #[cfg(test)]
