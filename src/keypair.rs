a7b0df8f3b9081cffa8be417293373f3f0c6e394
hollowness-inside <malikov.vp@students.dvfu.ru>
2024-07-24 07:47:31 +1000

    Create `Verifier` trait

--- src/keypair.rs
+++ src/keypair.rs
@@ -4,7 +4,21 @@ use crate::{
 };
 
 /// Trait defining the common functionalities of a public-private cryptography key pair.
-pub trait KeyPair: PublicKey + PrivateKey {}
+pub trait KeyPair: PublicKey + PrivateKey + Signer + Verifier {}
+
+pub trait PublicKey {
+    /// Encrypts a byte slice using the public key.
+    fn encrypt(&self, bytes: &[u8]) -> Result<Vec<u8>>;
+
+    fn encrypt_chunked(&self, bytes: &[u8], chunk_size: usize) -> Result<Vec<u8>> {
+        let content: Vec<Vec<_>> = bytes
+            .chunks(chunk_size)
+            .flat_map(|chunk| self.encrypt(chunk))
+            .collect();
+
+        Ok(marshal_bytes(&content))
+    }
+}
 
 pub trait PrivateKey {
     /// Decrypts an encrypted slice using the public key.
@@ -22,16 +36,27 @@ pub trait PrivateKey {
     }
 }
 
-pub trait PublicKey {
-    /// Encrypts a byte slice using the public key.
-    fn encrypt(&self, bytes: &[u8]) -> Result<Vec<u8>>;
+pub trait Signer {}
 
-    fn encrypt_chunked(&self, bytes: &[u8], chunk_size: usize) -> Result<Vec<u8>> {
-        let content: Vec<Vec<_>> = bytes
-            .chunks(chunk_size)
-            .flat_map(|chunk| self.encrypt(chunk))
-            .collect();
+pub trait Verifier {
+    fn verify(
+        &self,
+        expected: &[u8],
+        signed_data: &[u8],
+        hashf: fn(&[u8]) -> Vec<u8>,
+    ) -> Result<bool>;
 
-        Ok(marshal_bytes(&content))
+    fn verify_chunked(
+        &self,
+        expected: &[u8],
+        signed_data: &[u8],
+        hashf: fn(&[u8]) -> Vec<u8>,
+        chunk_size: usize,
+    ) -> Result<bool> {
+        Ok(unmarshal_bytes(signed_data)
+            .iter()
+            .zip(expected.chunks(chunk_size))
+            .flat_map(|(sig, exp)| self.verify(exp, sig, hashf))
+            .all(|b| b))
     }
 }
