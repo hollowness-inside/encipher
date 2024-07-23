d483584e0e252560471df4f27435696f2c5ee73a
hollowness-inside <malikov.vp@students.dvfu.ru>
2024-07-24 07:49:14 +1000

    Implement Verifier for RsaPublic

--- src/rsa/public.rs
+++ src/rsa/public.rs
@@ -1,6 +1,7 @@
 use ibig::UBig;
 use ibig_ext::powmod::PowMod;
 
+use crate::keypair::Verifier;
 use crate::result::Error;
 use crate::{keypair::PublicKey, result::Result};
 
@@ -25,5 +26,22 @@ impl PublicKey for RsaPublic {
 
         let message = message.powmod(self.exponent.clone(), &self.divisor);
         Ok(message.to_be_bytes())
+    }
+}
+
+impl Verifier for RsaPublic {
+    fn verify(
+        &self,
+        expected: &[u8],
+        signed_data: &[u8],
+        hashf: fn(&[u8]) -> Vec<u8>,
+    ) -> Result<bool> {
+        let expected_hash = hashf(expected);
+
+        let out = UBig::from_be_bytes(signed_data)
+            .powmod(self.exponent.clone(), &self.divisor)
+            .to_be_bytes();
+
+        Ok(out.eq(&expected_hash))
     }
 }
